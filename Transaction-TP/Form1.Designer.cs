c99c4a563d48c111d0394fa7c52ed3f5ce00a114
Acoriy <sofyane.acoriy@gmail.com>
2022-02-03 23:57:24 +0100

    Exécute deux requêtes en même temps. Ou pas en même temps

---
+++ Transaction-TP/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿
+namespace Transaction_TP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textcherche = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnEnregistrer = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(22, 187);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(668, 307);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // textcherche
+            // 
+            this.textcherche.Location = new System.Drawing.Point(333, 24);
+            this.textcherche.Name = "textcherche";
+            this.textcherche.Size = new System.Drawing.Size(242, 26);
+            this.textcherche.TabIndex = 1;
+            this.textcherche.TextChanged += new System.EventHandler(this.textcherche_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(116, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(186, 22);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Idclient expéditeur :";
+            // 
+            // btnEnregistrer
+            // 
+            this.btnEnregistrer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.btnEnregistrer.FlatAppearance.BorderSize = 0;
+            this.btnEnregistrer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEnregistrer.Image = global::Transaction_TP.Properties.Resources.Enregistrer;
+            this.btnEnregistrer.Location = new System.Drawing.Point(899, 283);
+            this.btnEnregistrer.Name = "btnEnregistrer";
+            this.btnEnregistrer.Size = new System.Drawing.Size(110, 93);
+            this.btnEnregistrer.TabIndex = 3;
+            this.btnEnregistrer.UseVisualStyleBackColor = false;
+            this.btnEnregistrer.Click += new System.EventHandler(this.btnEnregistrer_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(147, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Nom complete :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(215, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Solde :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(370, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(21, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "**";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(370, 143);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(21, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "**";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(767, 106);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(242, 26);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(767, 220);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(242, 26);
+            this.textBox2.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(785, 60);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(206, 22);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Le solde transformer :";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(814, 178);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(152, 22);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Le resepteure  :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(1080, 572);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnEnregistrer);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textcherche);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textcherche;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnEnregistrer;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Transaction-TP/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Transaction_TP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection cn = new SqlConnection(@"Data Source=.;Initial Catalog=Transactione;Integrated Security=True");
+        SqlCommand cmd = new SqlCommand();
+        SqlDataAdapter ad;
+        DataTable dt;
+        SqlTransaction tr;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmd.Connection = cn;
+            AfficherInfo();
+
+        }
+
+        private void textcherche_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textcherche.Text != "")
+                {
+                    cn.Open();
+                    cmd.CommandText = "select * from Client_tab where idC=" + int.Parse(textcherche.Text);
+                    SqlDataReader dr;
+                    dr =cmd.ExecuteReader();
+                    if (dr.HasRows)
+                    {
+                        while (dr.Read())
+                            
+                         {
+                            label4.Text = dr[1].ToString();
+                            label5.Text = dr[2].ToString();
+                            
+                        }
+                    }
+                    dr.Close();
+
+
+
+                }
+                else
+                {
+                    label4.Text = "**";
+                    label5.Text = "**";
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+
+        }
+        void AfficherInfo()
+        {
+            cmd = new SqlCommand("select *from Client_tab ", cn);
+            ad = new SqlDataAdapter(cmd);
+            dt = new DataTable();
+            ad.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void btnEnregistrer_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cn.Open();
+                // le dibut de tronsaction (bigin) :
+
+                tr = cn.BeginTransaction();
+                cmd.Transaction = tr;
+
+                cmd.CommandText = "update Client_tab set Solde-=@a where idC=@b";
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@a", textBox1.Text);
+                cmd.Parameters.AddWithValue("@b", textcherche.Text);
+
+                cmd.ExecuteNonQuery();
+
+                cmd.CommandText = "update Client_tabeer set Solde+=@a where idC=@c";
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@a", textBox1.Text);
+                cmd.Parameters.AddWithValue("@c", textBox2.Text);
+                cmd.ExecuteNonQuery();
+
+                //la validation des transaction :
+                tr.Commit();
+                AfficherInfo();
+               
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+                //Annuler la transaction :
+                tr.Rollback();
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
